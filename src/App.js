76e8d06793a726794462eb18b8a41f667007378c
carl2008 <s3753624@rmit.edu.vn>
2021-08-20 19:20:20 +0700

    Navigation Bar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './component/Navbar/Navbar';
+import AboutUs from './component/AboutUs';
+import Articles from './component/Articles';
+import Discussion from './component/Discussion';
+import Home from './component/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <br />
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route exact path="/AboutUs"><AboutUs /></Route>
+          <Route exact path="/Discussion"><Discussion /></Route>
+          <Route exact path="/Articles"><Articles /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/component/Discussion.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Discussion extends Component {
+    render () {
+        return (
+            <div>This is Discussion page</div>
+        )
+    }
+}
+
+export default Discussion
