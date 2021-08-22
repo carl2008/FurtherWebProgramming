56a86c38c143187fb62ee5aceab9fba598e2ba43
Hanh Doan <s3639869@rmit.edu.vn>
2021-08-22 18:12:55 +0700

    Add files for Discussions page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Navbar from './component/Navbar/Navbar';
 import AboutUs from './component/AboutUs';
 import Articles from './component/Articles';
-import Discussion from './component/Discussion';
+import DiscussionList from './component/DiscussionList';
 import Home from './component/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -16,7 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/"><Home /></Route>
           <Route exact path="/AboutUs"><AboutUs /></Route>
-          <Route exact path="/Discussion"><Discussion /></Route>
+          <Route exact path="/Discussion"><DiscussionList /></Route>
           <Route exact path="/Articles"><Articles /></Route>
         </Switch>
       </Router>
---
+++ src/component/Reply.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react"
+import './DiscussionList.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Reply({ id }) {
+    const [replies, setReplies] = useState([])
+    const endPoint = 'https://611fc518c772030017424085.mockapi.io/api/v1/topics/' + id + '/replies'
+
+    const loadReplies = () => {
+        fetch(endPoint)
+            .then(response => response.json())
+            .then(data => setReplies(data))
+    }
+
+    useEffect(() => {
+        loadReplies()
+    }, [])
+
+    return (
+        replies.map((reply, i) => {
+            return (
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <div class="media forum-item">
+                            <a href="javascript:void(0)" class="card-link">
+                                <img src={reply.avatar} class="rounded-circle" width="50" alt="User" />
+                                <small class="d-block text-center text-muted">Doctor</small>
+                            </a>
+                            <div class="media-body ml-3">
+                                <a href="javascript:void(0)" class="text-secondary">{reply.name}</a>
+                                <small class="text-muted ml-2">{(new Date(reply.createdAt)).toLocaleDateString('default', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })}</small>
+                                <div class="mt-3 font-size-sm">
+                                    <p>{reply.content}</p>
+                                </div>
+                                <button class="btn btn-xs text-muted has-icon"><i class="fa fa-heart" aria-hidden="true"></i>1</button>
+                                <a href="javascript:void(0)" class="text-muted small">Reply</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    )
+}
