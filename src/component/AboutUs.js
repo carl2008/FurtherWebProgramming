619a32d801b716e7d22bf7cb719d00ebdfd6352f
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-08-22 12:27:27 +0700

    Add About Us page contents

--- src/component/AboutUs.js
+++ src/component/AboutUs.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import ContactForm from './ContactForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class AboutUs extends Component {
     render () {
         return (
-            <div>This is About Us page</div>
+            <div className="container h-100">
+                <div className="row h-100">
+                    <div className="col-12 mx-auto">
+                        <div className="jumbotron">
+                            <h3><u>About This Page</u></h3><br></br>
+                            <p className="col-10 mx-auto">As of 2021, the COVID-19 pandemic situation is still a huge concern to everyone around the world. Due to the unclear symptoms of the virus, citizens have a lot of questions about the current circumstance or how to deal with a specific situation (Ex: How to treat F0 and F1 patients, how to self-isolate at home, etc.). Another reason is that face-to-face check-ups with doctors are unavailable, this could lead to some problems that citizens are unaware of (Ex: Mistaking common diseases’ symptoms with COVID-19 ones, etc.).
+                                <br></br>For those reasons, our team builds a Medical Consultation system which helps citizens and patients easily communicate with doctors anywhere and anytime they want to. The main purpose of the project is to make a website where doctors and patients can socialize by asking and answering questions regarding covid-19 virus. Not only can doctors answer questions posted by patients, but they can also post medical articles to inform as many people as possible.
+                            </p>
+                        </div>
+                        <div className="jumbotron">
+                            <h3><u>Register as a Doctor</u></h3><br></br>
+                            <p className="col-10 mx-auto">Please send us an email with all your licenses and certifications attached to we can verify your doctor's background and credentials.
+                            Email us at <a href="mailto: 123@gmail.com">123@gmail.com</a>.</p>
+                        </div>
+                        <div className="jumbotron">
+                            <ContactForm />
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
