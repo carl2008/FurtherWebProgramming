34c091752b75cb4f7680d888479da54fa66be7c1
Duc <s3788345@rmit.edu.vn>
2021-08-29 00:11:01 +0700

    add login page and update form for users

---
+++ src/component/RegisterForm/LogInForm.js
@@ -0,0 +1,64 @@
+import "./RegisterForm.css";
+import React, { useState , useEffect} from "react";
+import "./Bootstrap.css";
+
+export default function LogInForm() {
+    //use useState hook for input handlings React form
+    
+
+    const [username, setUsername] = useState([]);
+    const [password, setPassword] = useState([]);
+
+    //store data in our website database --We haven't decide our database yet so I just use localhost
+    const endPoint = "localhost:3000";
+
+    function LogIn() {
+        fetch(endPoint, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({  username: username, password: password}),
+        }).then((data) => {
+            fetch(endPoint).then((response) =>
+                response.json().then((data) => setUsername(data),setPassword(data))
+            );
+
+            data.json();
+        });
+    };
+
+    return (
+
+        <div className='container'>
+
+            
+            <form className="row g-3">
+            <div className="col-md-12">
+                <h1>Login</h1>
+            </div>
+            
+            <div className="col-md-12">
+                <input type="text" class="form-control" id="inputUsername" placeholder="Username" 
+                value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <br></br>
+            </div>
+
+            <div className="col-md-12">
+                <input type="password" class="form-control" id="inputPassword" placeholder="Create Password" 
+                value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <br></br>
+            </div>
+            
+            <div className="col-md-12">
+                <button className="btn btn-primary col-3 mx-auto" onClick={() => LogIn()}>
+                    Log In
+                </button>
+            </div>
+            </form>
+
+        </div>
+    
+
+    );
+}
--- src/component/RegisterForm/UpdateForm.js
+++ src/component/RegisterForm/UpdateForm.js
@@ -1,8 +1,8 @@
 import "./RegisterForm.css";
-import React, { useState } from "react";
+import React, { useState} from "react";
 import "./Bootstrap.css";
 
-export default function RegisterForm() {
+export default function UpdateForm() {
     //use useState hook for input handlings React form
     
     const [firstName, setFirstName] = useState([]);
@@ -10,36 +10,44 @@ export default function RegisterForm() {
     const [emailAddress, setEmailAddress] = useState([]);
     const [username, setUsername] = useState([]);
     const [password, setPassword] = useState([]);
+    const [specialties, setSpecialty] = useState([]);
+    const [introduction, setIntroduction] = useState([]);
 
 
     //store data in our website database --We haven't decide our database yet so I just use localhost
-    const endPoint = "localhost:3000";
+    // supposed this database is for doctors
+    const endPoint = "localhost:3000/doctor";
 
-    function save() {
+    function update() {
         fetch(endPoint, {
-            method: "POST",
+            method: "PUT",
             headers: {
                 "Content-type": "application/json",
             },
             body: JSON.stringify({ firstName: firstName , lastName:lastName , emailAddress: emailAddress,
-            username: username, password: password}),
+            username: username, password: password ,specialties: specialties,  introduction : introduction}),
         }).then((data) => {
             fetch(endPoint).then((response) =>
-                response.json().then((data) => setFirstName(data))
+                response.json().then((data) => setFirstName(data),setLastName(data),
+                 setEmailAddress(data),setUsername(data),setPassword(data),setSpecialty(data), setIntroduction(data))
             );
 
             data.json();
         });
     };
 
+    
+
+    
+
     return (
 
-        <div className='container' id="formBorder">
+        <div className='container'>
 
             
             <form className="row g-5">
             <div className="col-md-12">
-                <h1>Sign Up</h1>
+                <h1>Update</h1>
             </div>
             <div className="col-md-6">
                 <input type="text" class="form-control" id="inputFirstName" placeholder="FirstName"
@@ -66,14 +74,21 @@ export default function RegisterForm() {
             </div>
 
             <div className="col-md-12">
-                <input type="password" class="form-control" id="inputPassword" placeholder="Create Password" 
+                <input type="password" class="form-control" id="inputPassword" placeholder="Change Password" 
                 value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <br></br>
             </div>
+
+            <div className="cold-md-12" color="grey">
+                
+            
+            </div>
+                   
+
             
             <div className="col-md-12">
-                <button className="btn btn-primary col-3 mx-auto" onClick={() => save()}>
-                    Register
+                <button className="btn btn-primary col-3 mx-auto" onClick={() => update()}>
+                    Update
                 </button>
             </div>
             </form>
