70df91f4945db3d38139dd439cf1df8bd2c06c58
XcSteven <73635652+XcSteven@users.noreply.github.com>
2021-08-31 12:36:38 +0700

    Add 2 models like.js and comment.js

---
+++ server/models/like.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema
+
+const LikeSchema = new Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  article: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Article'
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_at'
+  }
+})
+
+module.exports = mongoose.model('Like', LikeSchema)
