a3406b065ab8aadf44378af38e9737fb29813459
Jihun Lee <s3753624@rmit.edu.vn>
2021-09-12 23:03:27 +0700

    InProgress

--- src/App.js
+++ src/App.js
@@ -13,34 +13,24 @@ import Home from './component/CovidAPI/Home';
 import Footer from './component/Navbar/Footer';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LogInForm from './component/RegisterForm/LogInForm';
-import UpdateForm from './component/RegisterForm/UpdateForm';
+// import UpdateForm from './component/RegisterForm/UpdateForm';
 // import UpdateFormDoctors from './component/RegisterForm/UpdateFormDoctors';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
-        <Switch>
-          <Route exact path="/"><NavigationBar /><br/><Home /></Route>
-          <Route exact path="/AboutUs"><NavigationBar /><br/><AboutUs /></Route>
-          <Route exact path="/Discussion"><NavigationBar /><br/><DiscussionList /></Route>
-          <Route exact path="/Discussion/new" component={DiscussionForm} />
-          <Route exact path="/Discussion/:id" component={DiscussionPost} />
-          <Route exact path="/Articles"><NavigationBar /><br/><Articles /></Route>
-          <Route exact path="/Article/:id" component={ArticlePost} />
-          <Route exact path="/LoginForm"><NavigationBar /><br/><LogInForm/></Route>
-          <Route exact path="/Register"><NavigationBar /><br/><RegisterForm/></Route>
+        <NavigationBar />
+        <br />
 
-          <Route exact path="/user"><NavbarLogin/><br/><Home /></Route>
-          <Route exact path="/user/AboutUs"><NavbarLogin /><br/><AboutUs /></Route>
-          <Route exact path="/user/Discussion"><NavbarLogin /><br/><DiscussionList /></Route>
-          <Route exact path="/user/Discussion/new" component={DiscussionForm} />
-          <Route exact path="/user/Discussion/:id" component={DiscussionPost} />
-          <Route exact path="/user/Articles"><NavbarLogin /><br/><Articles /></Route>
-          <Route exact path="/user/Article/:id" component={ArticlePost} />
-          <Route exact path="/user/LoginForm"><NavbarLogin /><br/><LogInForm/></Route>
-          <Route exact path="/user/Profile"><NavbarLogin /><br/><UpdateForm/></Route>
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route exact path="/AboutUs"><AboutUs /></Route>
+          <Route exact path="/Discussion"><DiscussionList /></Route>
+          <Route exact path="/Articles"><Articles /></Route>
+          <Route exact path="/Article/:id" component={ArticlePost} />
+          <Route exact path="/Login" component={LogInForm} />
+          <Route exact path="/Register" component={RegisterForm} />
         </Switch>
 
         <br />
