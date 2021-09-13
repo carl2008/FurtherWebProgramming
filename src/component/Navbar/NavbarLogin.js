96bd8daf63dff7a774167b5c7cb7f8e22c18af09
Do Hoang Duc <59699738+jasondohoang@users.noreply.github.com>
2021-09-13 21:39:42 +0700

    new log in form
    history push return null (not working)

--- src/component/Navbar/NavbarLogin.js
+++ src/component/Navbar/NavbarLogin.js
@@ -1,45 +1,55 @@
-import React, { Component } from 'react';
-import './Navbar.css';
-import logo from './logo.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar';
+import React, { Component } from "react";
+import "./Navbar.css";
+import logo from "./logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { useHistory } from "react-router-dom";
 
-class NavbarLogin extends Component {
-    render() {
-        return (
-            <div className="removing-space">
-                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="/">
-                            <img
-                                src={logo}
-                                width="35"
-                                height="30"
-                                className="d-inline-block align-top"
-                                alt="R-MED logo"
-                            />
-                        </Navbar.Brand>
-                        <Navbar.Brand href="/">R-MED</Navbar.Brand>
+export default function NavbarLogin() {
 
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="/AboutUs">About Us</Nav.Link>
-                                <Nav.Link href="/Discussion">Discussion</Nav.Link>
-                                <Nav.Link href="/Articles">Articles</Nav.Link>
-                            </Nav>
-                            <Nav className="login-register">
-                                <Nav.Link href="#">Log out</Nav.Link>
-                                <Nav.Link href="#">Profile</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        )
-    }
+     NavbarLogin = () => {
+      const history = useHistory();
+    
+
+    return (
+      <div className="removing-space">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/user">
+              <img
+                src={logo}
+                width="35"
+                height="30"
+                className="d-inline-block align-top"
+                alt="R-MED logo"
+              />
+            </Navbar.Brand>
+            <Navbar.Brand href="/user">R-MED</Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/user/AboutUs">About Us</Nav.Link>
+                <Nav.Link href="/user/Discussion">Discussion</Nav.Link>
+                <Nav.Link href="/user/Articles">Articles</Nav.Link>
+              </Nav>
+              <Nav className="login-register">
+                <Nav.Link
+                  onClick={() => {
+                    localStorage.removeItem("userInfo");
+                    history.push("/");
+                  }}
+                >
+                  Log out
+                </Nav.Link>
+                <Nav.Link href="/user/Profile">Profile</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
 }
-
-export default NavbarLogin
+}
--- src/component/RegisterForm/LogInForm.js
+++ src/component/RegisterForm/LogInForm.js
@@ -4,24 +4,34 @@ import "./Bootstrap.css";
 import axios from "axios";
 import Loading from "./Loading";
 import ErrorMessage from "./ErrorMessage";
+import { useHistory } from "react-router-dom";
 
-export default function LogInForm() {
+export default function LogInForm({history}) {
     //use useState hook for input handlings React form
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
+   const history2 = useHistory();
+
+    useEffect(() => {
+        const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        console.log("user is directed");
+        console.log(userInfo);
+        if(userInfo ){
+           history2.push("/user");
+           console.log('directed');
+      }
+   }, [history]);
+
+    // const handleLogin = () => {
+    //     const userInfo = localStorage.getItem("userInfo");
+
+    //     if(userInfo){
+    //        history.push("/user");
+    //   }
+    // }
     
-   // useEffect(() => {
-     //   const userInfo = localStorage.getItem("userInfo");
-
-   //     if(userInfo){
-
-  //          history.push("/mynotes");
-   //     }
-
-  //  }, [history]);
-
     const submitHandler = async (e) => {
         e.preventDefault();
 
@@ -30,12 +40,12 @@ export default function LogInForm() {
                 header: {
                     "Content-type": "application/json"
                 }
-
             }
+
             setLoading(true); 
 
             const { data } = await axios.post(
-                "/api/users/login",
+                "http://localhost:9000/api/users/login",
                 {
                     username,
                     password,
@@ -54,29 +64,31 @@ export default function LogInForm() {
     return (
 
         <div>
-            {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-            {loading && <Loading />}
+            
 
             <div className='container' id="formBorder">
+            {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+            {loading && <Loading />}
                 <form className="row g-3" id="form-styling1" onSubmit={submitHandler}>
                     <div className="col-md-12">
                         <h1 id="form-title">Login</h1>
                     </div>
 
                     <div className="col-md-12">
-                        <input type="text" class="form-control" id="inputUsername" placeholder="Username"
+                        <input type="text" className="form-control" id="inputUsername" placeholder="Username"
                             value={username} onChange={(e) => setUsername(e.target.value)} />
                         <br></br>
                     </div>
 
                     <div className="col-md-12">
-                        <input type="password" class="form-control" id="inputPassword" placeholder="Create Password"
+                        <input type="password" className="form-control" id="inputPassword" placeholder="Create Password"
                             value={password} onChange={(e) => setPassword(e.target.value)} />
                         <br></br>
                     </div>
 
                     <div className="col-md-12">
-                        <button className="btn btn-primary col-3 mx-auto" id="button-form">
+                        <button className="btn btn-primary col-3 mx-auto" id="button-form" >
+                            {/* onClick={() => handleLogin()} */}
                             Log In
                         </button>
                     </div>
