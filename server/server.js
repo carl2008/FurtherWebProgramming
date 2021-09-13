a31380293f5ec6d31d16a46906741688ba199c11
Jihun Lee <s3753624@rmit.edu.vn>
2021-09-13 08:03:21 +0700

    Registration Frontend Done

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 // import 
 require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-//from the video 
+app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
 
 //routers
 const articles = require('./router/articleRouter')
